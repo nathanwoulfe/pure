f5c64ec6c07d2d04cc45bd422d9a67a98d018abe
Nathan Woulfe <nathan@nathanw.com.au>
2019-11-21 16:31:19 +1000

    update umbraco

--- Pure/app_plugins/pure/backoffice/pure.js
+++ Pure/app_plugins/pure/backoffice/pure.js
@@ -27,18 +27,54 @@
         const activeClass = 'active';
         const appContent = document.querySelector('.umb-app-content');
 
-        const clearNav = () => appContent.removeChild(document.querySelector('.pure-anchors'));
-        const deselectNav = () => document.querySelector('.pure-anchor.active').classList.remove(activeClass);
+        /** */
+        const clearNav = () => {
+            for (let p of panels) {
+                observer.unobserve(p.querySelector('.umb-group-panel__header'));
+            }
+
+            appContent.removeChild(document.querySelector('.pure-anchors'));
+        };
+
+        /** */
+        const deselectNav = () => document.querySelector('.pure-anchor.active').classList.remove(activeClass);        
+
+        /**
+         * 
+         * @param {number} level
+         */
         const step = level => document.body.classList.toggle('pure-mode' + (level ? '--' + level : ''));
 
+        /** Deactivate Pure */
+        const close = () => {
+            step(2);
+
+            $timeout(() => step(1), duration);
+
+            $timeout(() => {
+                step(0);
+                clearNav();
+            }, duration * 2 + offset);
+        };
+
+        /** Activate Pure */
+        const open = () => {
+            step(0);
+            buildNav();
+
+            $timeout(() => step(1), offset);
+            $timeout(() => step(2), duration + offset);
+        };
+
+        /**
+         * 
+         * @param {any} entries
+         */
         const changeNav = entries => {
             for (let entry of entries) {
 
-                const anchor = entry.target.dataset.appAnchor;
+                const anchor = entry.target.parentNode.dataset.appAnchor;
                 const elm = document.querySelector(`[data-pure-target="${anchor}"]`);
-
-                if (!elm)
-                    continue;
 
                 if (entry.isIntersecting && entry.intersectionRatio >= threshold) {
                     elm.classList.add('is-visible');
@@ -51,8 +87,16 @@
             }
         };
 
+        /** */
+        const observer = new IntersectionObserver(changeNav, {
+            threshold: threshold,
+            rootMargin: '0px'
+        });
+
+        /** */
         const highlightActive = () => {
             const firstVisible = document.querySelector('.is-visible');
+
             deselectNav();
 
             if (firstVisible) {
@@ -64,13 +108,7 @@
             }
         };
 
-        const observer = new IntersectionObserver(changeNav, {
-            threshold: threshold
-        });
-
-        /*
-         *
-         */
+        /** */
         const buildNav = () => {
 
             let list = document.createElement('ul');
@@ -105,9 +143,7 @@
             appContent.insertBefore(list, appContent.firstElementChild);
         };
 
-        /*
-         * Toggle pure mode on SHIFT+P
-         */
+        /** */
         document.addEventListener('keydown', e => {
 
             if (e.shiftKey && e.keyCode === 80 && !animating) {
@@ -121,26 +157,15 @@
 
                 for (let p of panels) {
                     p.setAttribute('id', p.dataset.appAnchor);
-                    observer.observe(p);
+                    observer.observe(p.querySelector('.umb-group-panel__header'));
                 }
                 
                 container = document.querySelector('[data-element="editor-container"]');
 
                 if (!active) {
-                    step(0);
-                    buildNav();
-
-                    $timeout(() => step(1), offset);
-                    $timeout(() => step(2), duration + offset);
+                    open();
                 } else {
-                    step(2);
-
-                    $timeout(() => step(1), duration);
-
-                    $timeout(() => { 
-                        step(0);
-                        clearNav(); 
-                    }, duration * 2 + offset); 
+                    close();
                 }
 
                 $timeout(() => animating = false, duration + offset);
@@ -148,6 +173,16 @@
                 active = !active;
             }
         }); 
+
+        /** */
+        document.addEventListener('mouseup', e => {
+            if (active && (e.button === 3 || e.button === 4)) {
+                close();
+
+                $timeout(() => animating = false, duration + offset);
+                active = !active;
+            }
+        });
     }
 
     const component = {
--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Pure provides a simple, clean and distraction-free editing interface for content
 
 Intended for use when editing complex content types or writing long-form articles, Pure removes backoffice chrome to leave only the property editors.
 
+`SHIFT+P` switches between regular Umbraco 8 and the Pure editing experience.
+
+Pure works on any Umbraco 8 minor version, but don't let that stop you updating Umbraco to 8.latest...
